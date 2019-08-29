b1686ff7db8fdcd2631e170c4bd214414d2463d7
Christopher Bare <christopherbare@outlook.com>
2019-08-29 09:43:38 -0400

    changed things

--- src/main/java/com/example/timetracking/Controller/TimesheetController.java
+++ src/main/java/com/example/timetracking/Controller/TimesheetController.java
@@ -12,11 +12,12 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.util.List;
 
-@Controller
+@RestController
 public class TimesheetController {
 
     @Autowired
@@ -25,20 +26,13 @@ public class TimesheetController {
     @Autowired
     private TimesheetService timesheetService;
 
-    @GetMapping(value= {"/timesheet", "/"})
-    public String getTimesheetForm(Model model) {
-        model.addAttribute("timesheet", new Timesheet());
-        return "newTimesheet";
-    }
-
+   
     @PostMapping(value = "/timesheets")
-    public String submitTimesheetForm(@Valid Timesheet timesheet, BindingResult bindingResult, Model model) {
+    public String submitTimesheetForm(@Valid Timesheet timesheet, BindingResult bindingResult) {
         User user = userService.getLoggedInUser();
         if(!bindingResult.hasErrors()){
             timesheet.setUser(user);
             timesheetService.save(timesheet);
-            model.addAttribute("successMessage", "Timesheet successfully submitted!");
-            model.addAttribute("timesheet", new Timesheet());
 
         }
         return "newTimesheet";
--- src/main/java/com/example/timetracking/Model/User.java
+++ src/main/java/com/example/timetracking/Model/User.java
@@ -24,7 +24,7 @@ public class User {
     @Column(name = "user_id")
     private long id;
 
-    private int active;
+    private boolean active;
 
     @Length(min = 3, message = "Your username must have at least 3 characters")
     @Length(max = 15, message = "Your username cannot have more than 15 characters")
@@ -48,67 +48,4 @@ public class User {
     @CreationTimestamp
     private Date createdAt;
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public int getActive() {
-        return active;
-    }
-
-    public void setActive(int active) {
-        this.active = active;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public Set<Role> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Set<Role> roles) {
-        this.roles = roles;
-    }
-
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
 }
